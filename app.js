bc39d3b6129c5bff81270f92950b923db9239a3a
Victor Listapad <victorlistapadjob@gmail.com>
2022-03-02 20:45:30 -0500

    added logic to create todo layout and to print author name. Printed all todos to a layout.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // ************************************************************************************************
 let todos = [];
 let users = [];
+const todoList = document.getElementById(`todoList`);
 // ************************************************************************************************
 // EVENT ATTACHMENT
 // ************************************************************************************************
@@ -13,16 +14,39 @@ document.addEventListener(`DOMContentLoaded`, appInit);
 function appInit() {
   Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
     [todos, users] = values;
-    console.log(todos);
+    todos.forEach((todo) => printTodo(todo));
     console.log(users);
   });
 }
 // ************************************************************************************************
 // BASIC LOGIC
 // ************************************************************************************************
+function printTodo({ userId, id, title, completed }) {
+  const li = document.createElement(`li`);
+  li.dataset.id = id;
+  li.innerHTML = ` <span>${title} <i>by</i> <b>${getUserName(
+    userId
+  )}</b></span> `;
+  const status = document.createElement(`input`);
+  status.type = `checkbox`;
+  status.checked = completed;
+  if (status.checked) {
+    li.classList.add(`crossedOut`);
+  }
+  const deleteFromList = document.createElement(`span`);
+  deleteFromList.innerHTML = `&times;`;
+  deleteFromList.classList.add(`close`);
+  li.append(deleteFromList);
+  li.prepend(status);
+  todoList.prepend(li);
+}
 
+function getUserName(userId) {
+  const singleUser = users.find((user) => user.id === userId);
+  return singleUser.name;
+}
 // ************************************************************************************************
-// ASYNC FUNCTIONALITY
+// ASYNC LOGIC
 // ************************************************************************************************
 async function getAllTodos() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
