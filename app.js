176011cb04eea99189846196d2057d9843b99348
Victor Listapad <victorlistapadjob@gmail.com>
2022-03-02 21:37:14 -0500

    Added async logic to update todo complete status on server, event listener on checkboxes to change status. Added async logic to post new todos and add them to a todo list.

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ let todos = [];
 let users = [];
 const todoList = document.getElementById(`todoList`);
 const userOptions = document.getElementById(`userOptions`);
-console.log(userOptions);
+const form = document.querySelector(`form`);
 // ************************************************************************************************
 // EVENT ATTACHMENT
 // ************************************************************************************************
 document.addEventListener(`DOMContentLoaded`, appInit);
+form.addEventListener(`submit`, handleSubmit);
 // ************************************************************************************************
 // EVENT LOGIC
 // ************************************************************************************************
@@ -19,6 +20,23 @@ function appInit() {
     todos.forEach((todo) => printTodo(todo));
     users.forEach((user) => printUserOption(user));
   });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  createTodo({
+    userId: Number(form.user.value),
+    title: form.todo.value,
+    completed: false,
+  });
+  form.todo.value = "";
+}
+
+function handleChange() {
+  todoId = this.parentElement.dataset.id;
+  completed = this.checked;
+  this.parentElement.classList.toggle(`crossedOut`);
+  updateStatus(todoId, completed);
 }
 // ************************************************************************************************
 // BASIC LOGIC
@@ -35,6 +53,7 @@ function printTodo({ userId, id, title, completed }) {
   if (status.checked) {
     li.classList.add(`crossedOut`);
   }
+  status.addEventListener(`change`, handleChange);
   const deleteFromList = document.createElement(`span`);
   deleteFromList.innerHTML = `&times;`;
   deleteFromList.classList.add(`close`);
@@ -54,6 +73,7 @@ function printUserOption({ id, name }) {
   option.value = id;
   userOptions.append(option);
 }
+
 // ************************************************************************************************
 // ASYNC LOGIC
 // ************************************************************************************************
@@ -67,4 +87,31 @@ async function getAllUsers() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
   const data = await res.json();
   return data;
+}
+
+async function createTodo(todo) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`, {
+    method: "POST",
+    body: JSON.stringify(todo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const newTodo = await res.json();
+  printTodo(newTodo);
+}
+
+async function updateStatus(todoId, completed) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      method: `PATCH`,
+      body: JSON.stringify({ completed }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+  console.log(data);
 }
